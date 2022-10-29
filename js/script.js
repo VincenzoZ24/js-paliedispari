e8b952f5621334a87d382dbf5ee90fbc3da712cd
VincenzoZ24 <zitov287@gmail.com>
2022-10-29 13:13:11 +0200

    definite variabili e function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 let parola = prompt("inserisci una parola");
+console.log("la parola è", parola);
+let parolaInversa = invertiParola(parola);
+
+if (parola == parolaInversa) {
+    console.log('la parola è palindroma');
+} else {
+    console.log('la parola non è palindroma');
+}
+
+function invertiParola(str) {
+    let strInversa = str.split('').reverse().join('');
+    return strInversa;
+};
